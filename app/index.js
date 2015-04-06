bed0b58ada37b604b04efbc1991893dc12f69aa3
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-06 23:30:52 +0000

    add reactive function to count all the todos in different categories

--- app/index.js
+++ app/index.js
@@ -11,6 +11,17 @@ app.on('model', function(model) {
   model.fn('all',     function(item) { return true; });
   model.fn('completed', function(item) { return item.completed;});
   model.fn('active',    function(item) { return !item.completed;});
+
+  model.fn('counters', function(todos) {
+    var counters = {active: 0, completed: 0 };
+    for (var id in todos) {
+      if(todos[id].completed) counters.completed++;
+      else counters.active++;
+      return counters;
+    }
+  });
+
+
 });
 
 app.get('/', getPage('all'));
@@ -21,6 +32,7 @@ function getPage(filter) {
   return function(page, model) {
     model.subscribe('todos', function() {
       model.filter('todos', filter).ref('_page.todos');
+      model.start('_page.counters', 'todos', 'counters');
       page.render();
     });
   }
@@ -40,3 +52,36 @@ app.proto.addTodo = function(newTodo) {
 app.proto.deleteTodo = function(todoId) {
   this.model.del('todos.' + todoId);
 };
+
+app.proto.clearCompleted = function() {
+  var todos = this.model.get('todos');
+
+  for (var id in todos) {
+    if (todos[id].completed) this.model.del('todos.'+id);
+  }
+}
+
+app.proto.editTodo = function(todo) {
+  this.model.set('_page.edit', {
+    id: todo.id,
+    text: todo.text
+  });
+
+  window.getSelection().removeAllRanges();
+  document.getElementById(todo.id).focus();
+}
+
+app.proto.doneEditing = function(todo) {
+  this.model.set('todos.'+todo.id+'.text', todo.text);
+  this.model.set('_page.edit', {
+    id: undefined,
+    text:''
+  });
+}
+
+app.proto.cancelEditing = function(e) {
+  // 27 = ESQ-key
+  if (e.keyCode == 27) {
+    this.model.set('_page.edit.id', undefined)
+  }
+}
