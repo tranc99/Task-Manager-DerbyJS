586dc27f02f80446306572e2966c577f358222ff
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-06 18:27:47 +0000

    initial commit

---
+++ app/index.js
@@ -0,0 +1,27 @@
+var derby = require('derby');
+var app = module.exports = derby.createApp('todos', __filename);
+
+//make app global for access in the console
+global.app = app;
+
+app.loadViews(__dirname+'/views');
+app.loadStyles(__dirname+'/css');
+
+app.on('model', function(model) {
+  model.fn('all',     function(item) { return true; });
+  model.fn('completed', function(item) { return item.completed;});
+  model.fn('active',    function(item) { return !item.completed;});
+});
+
+app.get('/', getPage('all'));
+app.get('/active', getPage('active'));
+app.get('/completed', getPage('completed'));
+
+function getPage(filter) {
+  return function(page, model) {
+    model.subscribe('todos', function() {
+      model.filter('todos', filter).ref('_page.todos');
+      page.render();
+    });
+  }
+}
